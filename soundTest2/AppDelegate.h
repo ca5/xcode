f28739e595ad24cce3debc53170953980fcae554
ca54makske <ca5@ca54mac2.local>
2012-05-28 16:42:37 +0900

    Initial Commit

---
+++ soundTest2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  soundTest2
+//
+//  Created by 籠島 啓介 on 12/05/28.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ soundTest2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  soundTest2
+//
+//  Created by 籠島 啓介 on 12/05/28.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
