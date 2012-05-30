7b09fc204657fabceef45f9772c1e7076c3d8613
Ca5 <ca54makske@yahoo.co.jp>
2012-05-31 01:21:29 +0900

    modify

---
+++ soundTest2/AudioQueueSoundPlayer.h
@@ -0,0 +1,13 @@
+//
+//  AudioQueueSoundPlayer.h
+//  soundTest2
+//
+//  Created by 籠島 啓介 on 12/05/29.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AudioQueueSoundPlayer : NSObject
+
+@end
---
+++ soundTest2/AudioQueueSoundPlayer.m
@@ -0,0 +1,13 @@
+//
+//  AudioQueueSoundPlayer.m
+//  soundTest2
+//
+//  Created by 籠島 啓介 on 12/05/29.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "AudioQueueSoundPlayer.h"
+
+@implementation AudioQueueSoundPlayer
+
+@end
